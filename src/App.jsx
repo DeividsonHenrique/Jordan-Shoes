d22fa889fda513126eddc9602737e1ea08fbd308
Bowie <deividson.henrique2016@hotmail.com>
2024-08-23 23:39:21 -0300

    jordan shoes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Index from './Components/Index'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Index />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Index/index.jsx
@@ -0,0 +1,100 @@
+function Index() {
+  return (
+    <>
+      <body>
+        <section className="top_fixed">Frete Gratis para todo Brasil</section>
+
+        <section className="hero">
+          <div className="hero_content">
+            <h1>
+              <img src="public/images/logo-jordan.svg" alt="" />
+              Jordan Shoes
+            </h1>
+
+            <h2>A melhor loja de Jordan</h2>
+            <p>
+              O tênis Jordan é fruto de uma velha e forte <br /> parceria entre
+              a nike e o jogador Michael Jordan.
+            </p>
+          </div>
+        </section>
+
+        <main>
+          <section className="produtos">
+            <div className="heading">
+                <h3>Os melhores em só lugar</h3>
+                <p>
+                A marca Jordan na JordanShoes é a escolha certa para os amanetes
+                sneakers que buscam estilo e conforto.
+                </p>
+            </div>
+          
+    
+          <section className="lista_produtos">
+
+            <div className="card">
+              <figure>
+                <img
+                    src="public/images/Shoes/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.png"
+                    alt=""
+                />
+              </figure>
+              <div className="card_detalhes">
+                <h4>Air Jordan 1 high zoom CMFT Tropical Twist</h4>
+                <h5>Tênis Air Jordan</h5>
+              </div>
+              <h6>1.049,00</h6>
+            </div>
+
+            <div className="card">
+              <figure>
+                <img
+                    src="public/images/Shoes/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.png"
+                    alt=""
+                />
+              </figure>
+              <div className="card_detalhes">
+                <h4>Air Jordan 1 high zoom CMFT Tropical Twist</h4>
+                <h5>Tênis Air Jordan</h5>
+              </div>
+              <h6>1.049,00</h6>
+            </div>
+            
+            <div className="card">
+              <figure>
+                <img
+                    src="public/images/Shoes/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.png"
+                    alt=""
+                />
+              </figure>
+              <div className="card_detalhes">
+                <h4>Air Jordan 1 high zoom CMFT Tropical Twist</h4>
+                <h5>Tênis Air Jordan</h5>
+              </div>
+              <h6>1.049,00</h6>
+            </div>
+
+            <div className="card">
+              <figure>
+                <img
+                    src="public/images/Shoes/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.png"
+                    alt=""
+                />
+              </figure>
+              <div className="card_detalhes">
+                <h4>Air Jordan 1 high zoom CMFT Tropical Twist</h4>
+                <h5>Tênis Air Jordan</h5>
+              </div>
+              <h6>1.049,00</h6>
+            </div>
+
+
+          </section>
+          </section>
+        </main>
+      </body>
+    </>
+  );
+}
+
+export default Index;
