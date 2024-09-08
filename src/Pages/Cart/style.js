6d7c5d7cf70b243635e1d3cf135f365adf008d67
Deividson Henry <109252541+DeividsonHenrique@users.noreply.github.com>
2024-09-07 21:17:35 -0300

    day 07.09.24

--- src/Pages/Cart/style.js
+++ src/Pages/Cart/style.js
@@ -1,47 +1,49 @@
 import styled from "styled-components";
 
 export const Carrinho = styled.section`
-    display: block; /* block */
-    padding-top: 30px;
-    width: 100%;
-    height: 100%;
-    margin-block: 20px;
-    padding-inline: 20px;
+  display: block; /* block */
+  padding-top: 30px;
+  width: 100%;
+  height: 100%;
+  margin-block: 20px;
+  padding-inline: 20px;
 
-    h2 {
+  h2 {
     margin-bottom: 10px;
-}
-`
+  }
+`;
 
 export const Table = styled.table`
-    width: 100%;
-    border-collapse: collapse;
+  width: 100%;
+  border-collapse: collapse;
 
-    tr, th, td {
-        border: 1px solid #ccc;
-        height: 40px;
-    }
+  tr,
+  th,
+  td {
+    border: 1px solid #ccc;
+    height: 40px;
+  }
 
-    td {
-        padding-inline: 10px;
-    }
-`
+  td {
+    padding-inline: 10px;
+  }
+`;
 
 export const ColunaApagar = styled.td`
-    width: 65px;
-    text-align: center;
+  width: 65px;
+  text-align: center;
 
-    span:hover {
-        cursor: pointer;
-        color: red;
-    }
-`
+  span:hover {
+    cursor: pointer;
+    color: red;
+  }
+`;
 
 export const ColunaApagarTh = styled.th`
-    width: 65px;
-    text-align: center;
-`
+  width: 65px;
+  text-align: center;
+`;
 
 export const Tfoot = styled.tfoot`
-    text-align: right;
-`
+  text-align: right;
+`;
